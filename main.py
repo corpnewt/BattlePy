ad0f0c05e866acb0ef87abc10a78977bfc1f36b3
CorpNewt <corporatenewt@gmail.com>
2018-01-18 16:11:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -16,13 +16,38 @@ class Game():
         self.playing = False
         self.shots   = 0
 
-p1 = player.Player()
+        
+    def one_player(self):
+        self.playing = True
+        p1 = player.Player()
+        p1.place_ships()
 
-p1.place_ships()
+        p2 = player.Player(num=2, bot=True)
 
-p2 = player.Player(num=2, bot=True)
+        while True:
+            p1.take_shot(p2)
+            # Check for a winner
+            if p2.check_end():
+                utils.cls()
+                utils.head("Player 1 Won!")
+                print(" ")
+                print("You are victorious!")
+                print(" ")
+                input("Press [enter] to continue...")
+                return
+            p2.take_shot(p1)
+            # Check for a winner
+            if p1.check_end():
+                utils.cls()
+                utils.head("Player 1 Lost!")
+                print(" ")
+                print("You have been defeated!")
+                print(" ")
+                input("Press [enter] to continue...")
+                return
 
-while True:
-    p2.take_shot(p1)
+g = Game()
+                
+g.one_player()
 
 input("Press [enter] to quit...")
